3dceb241b79c548c26b2ad288fc9d2d16f190939
C$ <crystaljqian@gmail.com>
2020-11-20 20:31:32 -0800

    Add initial template.

---
+++ guess_who.js
@@ -0,0 +1,10 @@
+var DIRECTORY = "img/";
+var EXTENSION = '.png';
+
+var images = [];
+for (var i = 0; i < 25; i++) {
+	images.push(DIRECTORY + i + EXTENSION)
+};
+
+console.log(images);
+console.log("HELLO");
