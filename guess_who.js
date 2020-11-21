606143c3fdef4a3a693f96eab6bf283c0c6bcc84
Jacob Stimes <jacob.stimes@gmail.com>
2020-11-20 21:19:06 -0800

    Initial grid

--- guess_who.js
+++ guess_who.js
@@ -3,15 +3,22 @@ const VERSION = 'v1/';
 const BLACK_IMAGE = DIRECTORY + 'black.jpg';
 const EXTENSION = '.jpg';
 const N_IMAGES = 24;
-var images = [];
-for (var i = 0; i < N_IMAGES; i++) {
-	image_path = DIRECTORY + VERSION + i + EXTENSION;
+const COLS = 6;
+const ROWS = N_IMAGES / COLS;
 
-	images.push(image_path);
-	document.getElementById('container').innerHTML +=
-		`<div class='card'>
-			<img class='image' src=${image_path}>
-		</div>`;
+let images = [];
+for (let row = 0; row < ROWS; row++) {
+	for (let col = 0; col < COLS; col++) {
+		index = row * COLS + col;
+		image_path = DIRECTORY + VERSION + index + EXTENSION;
+
+		images.push(image_path);
+		document.getElementById('container').innerHTML +=
+			`<span class='card'>
+				<img class='image' src=${image_path}>
+			</span>`;
+	}
+	document.getElementById('container').innerHTML += `<br>`;
 };
 $('.card').click(function () {
 	$(this).toggleClass('flipped');
