e22ed56a2cdeece56c1d6b29ab453afef7075239
C$ <crystaljqian@gmail.com>
2020-11-20 21:03:37 -0800

    Added style, jQuery and working on toggle black.

--- guess_who.js
+++ guess_who.js
@@ -1,17 +1,24 @@
-var DIRECTORY = "img/";
-var EXTENSION = '.png';
-
+console.log("BEGINNING");
+var DIRECTORY = "./img/";
+var VERSION = "v1/";
+var BLACK_IMAGE = DIRECTORY + "black.jpg";
+var EXTENSION = '.jpg';
+var N_IMAGES = 24;
+console.log("ASDFASDF");
 var images = [];
-for (var i = 0; i < 25; i++) {
-	image_path = DIRECTORY + i + EXTENSION;
+for (var i = 0; i < N_IMAGES; i++) {
+	image_path = DIRECTORY + VERSION + i + EXTENSION;
+
 	images.push(image_path);
-	
 	document.getElementById('container').innerHTML +=
 		`<div><a href=${image_path}>
-		<img src=${image_link}/>
+		<img class='card' src=${image_path}>
 		</a></div>`;
-
 };
+console.log("TESTING");
+$(".card").click(function(){
+	$(this).toggleClass("flipped");
+});
 
+console.log('asdf');
 console.log(images);
-console.log("HELLO");
