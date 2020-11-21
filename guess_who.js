d6b95dce00ed6ae5c66464b4564195c82efda702
Jacob Stimes <jacob.stimes@gmail.com>
2020-11-20 21:12:47 -0800

    Click to toggle card

--- guess_who.js
+++ guess_who.js
@@ -1,23 +1,25 @@
-console.log("BEGINNING");
-var DIRECTORY = "./img/";
-var VERSION = "v1/";
-var BLACK_IMAGE = DIRECTORY + "black.jpg";
+console.log('BEGINNING');
+var DIRECTORY = './img/';
+var VERSION = 'v1/';
+var BLACK_IMAGE = DIRECTORY + 'black.jpg';
 var EXTENSION = '.jpg';
 var N_IMAGES = 24;
-console.log("ASDFASDF");
+console.log('ASDFASDF');
 var images = [];
 for (var i = 0; i < N_IMAGES; i++) {
 	image_path = DIRECTORY + VERSION + i + EXTENSION;
 
 	images.push(image_path);
 	document.getElementById('container').innerHTML +=
-		`<div>
-		<img class='card' src=${image_path}>
-		</a></div>`;
+		`<div class='card'>
+			<img class='image' src=${image_path}>
+		</div>`;
 };
-console.log("TESTING");
-$(".card").click(function(){
-	$(this).toggleClass("flipped");
+console.log('TESTING');
+$('.card').click(function () {
+	$(this).toggleClass('flipped');
+	$(this).children().toggleClass('flipped')
+
 });
 
 console.log('asdf');
