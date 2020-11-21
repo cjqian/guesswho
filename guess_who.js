0a742ef7e4801f7e1f2d0d4d7afb6088a2edcec5
Jacob Stimes <jacob.stimes@gmail.com>
2020-11-20 21:24:29 -0800

    Clean grid

--- guess_who.js
+++ guess_who.js
@@ -7,19 +7,23 @@ const COLS = 6;
 const ROWS = N_IMAGES / COLS;
 
 let images = [];
+let html = `<div class='grid'>`;
 for (let row = 0; row < ROWS; row++) {
+	html += `<div class='row'>`;
 	for (let col = 0; col < COLS; col++) {
 		index = row * COLS + col;
 		image_path = DIRECTORY + VERSION + index + EXTENSION;
 
 		images.push(image_path);
-		document.getElementById('container').innerHTML +=
-			`<span class='card'>
-				<img class='image' src=${image_path}>
-			</span>`;
+		html += `<span class='card'>
+					<img class='image' src=${image_path}>
+				</span>`;
 	}
-	document.getElementById('container').innerHTML += `<br>`;
-};
+	html += `</div>`;
+}
+html += `</div>`;
+document.getElementById('container').innerHTML = html;
+
 $('.card').click(function () {
 	$(this).toggleClass('flipped');
 	$(this).children().toggleClass('flipped')
