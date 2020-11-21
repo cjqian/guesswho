28b337bfcdfde72ec12fd095b4f42aaec1de32ba
C$ <crystaljqian@gmail.com>
2020-11-20 21:21:29 -0800

    Add my person

--- guess_who.js
+++ guess_who.js
@@ -1,26 +1,42 @@
-console.log('BEGINNING');
 var DIRECTORY = './img/';
 var VERSION = 'v1/';
 var BLACK_IMAGE = DIRECTORY + 'black.jpg';
 var EXTENSION = '.jpg';
 var N_IMAGES = 24;
-console.log('ASDFASDF');
 var images = [];
-for (var i = 0; i < N_IMAGES; i++) {
-	image_path = DIRECTORY + VERSION + i + EXTENSION;
 
-	images.push(image_path);
-	document.getElementById('container').innerHTML +=
+function getImageFromIndex(i) {
+	var image_path = DIRECTORY + VERSION + i + EXTENSION;
+	return image_path;
+};
+
+function loadImages() {
+	for (var i = 0; i < N_IMAGES; i++) {
+		image_path = getImageFromIndex(i);
+		images.push(image_path);
+		document.getElementById('container').innerHTML +=
+			`<div class=''>
+				<img class='image' src=${image_path}>
+			</div>`;
+	};
+};
+loadImages();
+
+// Load .myPerson with a random person.
+function loadMyPerson() {
+	var idx = Math.floor(Math.random() * N_IMAGES);
+	var image_path = getImageFromIndex(idx);
+
+	document.getElementById('myPerson').innerHTML +=
 		`<div class='card'>
 			<img class='image' src=${image_path}>
 		</div>`;
+
 };
-console.log('TESTING');
+loadMyPerson();
+
 $('.card').click(function () {
 	$(this).toggleClass('flipped');
 	$(this).children().toggleClass('flipped')
 
 });
-
-console.log('asdf');
-console.log(images);
